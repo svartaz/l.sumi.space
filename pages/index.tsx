079ee72fe59c8fa807897fb583923c9f446e750d
sumi <u22696@gmail.com>
2022-12-08 11:38:08 +0900

    fix ul

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,22 +17,19 @@ const Me = () => <>
     <ul>
       <li>修士 (情報科學)</li>
       <li>軟件工程師</li>
-      <li>語學趣味者
+      <li>語學趣味者</li>
+      <ul>
+        <li>EN: B2</li>
         <ul>
-          <li>EN: B2
-            <ul>
-              <li>英檢 準1級 2555 (CE2021)</li>
-              <li>TOEIC L&R 900 (CE2022)</li>
-            </ul>
-          </li>
-          <li>CMN: A2
-            <ul>
-              <li>HSK 4級 235 (CE2021)</li>
-            </ul>
-          </li>
-          <li>FR, FA, YUE, JBO: A1未滿</li>
+          <li>英檢 準1級 2555 (CE2021)</li>
+          <li>TOEIC L&R 900 (CE2022)</li>
         </ul>
-      </li>
+        <li>CMN: A2</li>
+        <ul>
+          <li>HSK 4級 235 (CE2021)</li>
+        </ul>
+        <li>FR, FA, YUE, JBO: A1未滿</li>
+      </ul>
       <li>近代自由主義者</li>
       <li>男性愛者</li>
     </ul>
@@ -130,20 +127,23 @@ const Fav = () => <>
 </>
 
 const Misc = () => <ul>
-  <li>舊網站
-    <ul>
-      <li><Link href='https://0.sumi.space'>CE2018 (Gulp, Pug)</Link></li>
-      <li><Link href='https://www.sumi.space'>CE2021 (Next.js)</Link></li>
-    </ul>
-  </li>
+  <li>舊網站</li>
+  <ul>
+    <li><Link href='https://0.sumi.space'>CE2018 (Gulp, Pug)</Link></li>
+    <li><Link href='https://www.sumi.space'>CE2021 (Next.js)</Link></li>
+  </ul>
   <li><Link href='https://sjagci.pages.dev'>將棋 (Elm)</Link></li>
   <li><Link href='https://tung.sumi.space'>ラテン化</Link></li>
-  <li>original characters
-    <ul>
-      <li><Link href='char/1'>suzuli, mitulu</Link></li>
-      <li><Link href='char/2'>lon</Link></li>
-    </ul>
-  </li>
+  <li>original characters</li>
+  <ul>
+    <li><Link href='char/1'>suzuli, mitulu</Link></li>
+    <li><Link href='char/2'>lon</Link></li>
+  </ul>
+  <li>links</li>
+  <ul>
+    <li><a href='https://booklog.jp/users/xek'>booklog</a></li>
+    <li><a href='https://eiga.com/user/1115495/review/'>eiga.com</a></li>
+  </ul>
 </ul>
 
 export async function getStaticProps() {
